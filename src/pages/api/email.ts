f2fda67b0f7726d11f480578bd3662b5d24b89a5
CristianoV <cristianoviieira@gmail.com>
2023-01-26 20:14:15 -0300

    feat: adiciona dotenv

--- src/pages/api/email.ts
+++ src/pages/api/email.ts
@@ -1,6 +1,9 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import nodemailer from 'nodemailer';
 
+import * as dotenv from 'dotenv'
+dotenv.config()
+
 var transport = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: 2525,
