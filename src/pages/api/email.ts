413244d4325ca6f7d4970cdf9b6e8d79259d28ae
CristianoV <cristianoviieira@gmail.com>
2023-01-26 17:46:05 -0300

    fix: try e catch transformado em callback

--- src/pages/api/email.ts
+++ src/pages/api/email.ts
@@ -12,31 +12,29 @@ var transport = nodemailer.createTransport({
 
 const sendEmail = (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
-    try {
-      const { name, email, message } = req.body as {
-        name: string;
-        email: string;
-        message: string;
-      };
+    const { name, email, message } = req.body as {
+      name: string;
+      email: string;
+      message: string;
+    };
 
-      if (!name || !email || !message) {
-        res.status(400).json({ message: 'Dados inválidos' });
-        return;
-      }
-
-      const mailOptions = {
-        from: email,
-        to: 'cristianoviieira@gmail.com',
-        subject: name,
-        text: message,
-      };
-
-      transport.sendMail(mailOptions);
-
-      res.status(200).json({ message: 'E-mail enviado com sucesso' });
-    } catch (error) {
-      res.status(500).json({ message: 'Erro ao enviar e-mail' });
+    if (!name || !email || !message) {
+      res.status(400).json({ message: 'Dados inválidos' });
+      return;
     }
+
+    const mailOptions = {
+      from: email,
+      to: 'cristianoviieira@gmail.com',
+      subject: name,
+      text: message,
+    };
+
+    transport.sendMail(mailOptions, (error) => {
+      if (error) res.status(500).json({ message: 'Erro ao enviar e-mail' });
+    });
+
+    res.status(200).json({ message: 'E-mail enviado com sucesso' });
   } else {
     res.setHeader('Allow', 'POST');
     res.status(405).end('Method not allowed');
