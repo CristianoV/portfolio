ebbcbd594279ce1f1e4a173d27848f31a0a7ee71
CristianoV <cristianoviieira@gmail.com>
2023-01-26 20:36:51 -0300

    Update axios.ts

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const fetchFromApi = axios.create({
-  baseURL: 'http://portfolio-two-opal-82.vercel.app/api',
+  baseURL: 'https://portfolio-2o1l38qb6-cristianov.vercel.app/api',
 });
