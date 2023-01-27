ad36726e4f4e23c1b1e5a223faae4d00ed16d80c
CristianoV <cristianoviieira@gmail.com>
2023-01-26 21:42:02 -0300

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app'
 import '../styles/globals.scss'
 import { Header } from '../components/Header'
+require('dotenv').config();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
