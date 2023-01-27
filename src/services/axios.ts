f201a28c2785aa75a529aeb432e61012803c88e3
CristianoV <cristianoviieira@gmail.com>
2023-01-26 21:31:09 -0300

    Update axios.ts

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,4 +1,8 @@
 import axios from 'axios';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const site = `${process.env.VERCEL_URL}/api`
 
 export const fetchFromApi = axios.create({
   baseURL: 'https://portfolio-2o1l38qb6-cristianov.vercel.app/api',
