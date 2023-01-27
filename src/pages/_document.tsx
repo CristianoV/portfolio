a2e4a5087e2a6e07bc23d266e0cef46fe71a2895
CristianoV <cristianoviieira@gmail.com>
2023-01-27 11:16:10 -0300

    fix: alterando linguagem do site

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang='en'>
+    <Html lang='pt-BR'>
       <Head>
         <link
           href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css'
