38c5d50d23f7149569b13bdd96a0a9ebeeea3689
CristianoV <cristianoviieira@gmail.com>
2023-01-27 00:40:16 -0300

    feat: altera url

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -3,5 +3,5 @@ import axios from 'axios';
 const site = process.env.NEXT_PUBLIC_VERCEL_URL || 'http://localhost:3000'
 
 export const fetchFromApi = axios.create({
-  baseURL: 'https://portfolio-nine-zeta-17.vercel.app',
+  baseURL: 'https://cristiano-portfolio.vercel.app',
 });
