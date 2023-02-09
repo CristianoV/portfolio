e3b28602332a29831b4bd7faf4d89188cf667a5f
CristianoV <cristianoviieira@gmail.com>
2023-02-09 19:03:57 -0300

    feat: adicina novo layout de header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,32 +1,23 @@
 import styles from './styles.module.scss';
-import Image from 'next/image';
-// import logo from '../../../public/images/logo.svg';
 
 import { ActiveLink } from '../ActiveLink';
 import Link from 'next/link';
 
 export function Header() {
   return (
-    <header className={styles.headerContainer}>
-      <div className={styles.headerContent}>
-        {/* <Link href='/'>
-          <Image src={logo} alt='Sujeito Programador Logo' />
-        </Link> */}
+    <header className={styles.container}>
+      <h1>
+        CristianoV<span>.</span>
+      </h1>
 
-        <nav>
-          <ActiveLink href='/' texto='Home' />
-          <ActiveLink href='/publicacoes' texto='Publicações' />
-          <ActiveLink href='/habilidades' texto='Habilidades' />
-        </nav>
-
-        <Link
-          className={styles.readyButton}
-          type='button'
-          href='/contato'
-        >
-          Contato
+      <nav>
+        <ActiveLink href='/' texto='Início' />
+        <ActiveLink href='/publicacoes' texto='Publicações' />
+        <ActiveLink href='/habilidades' texto='Habilidades' />
+        <Link className={styles.readyButton} type='button' href='/contato'>
+          <p>ME CONTRATE</p>
         </Link>
-      </div>
+      </nav>
     </header>
   );
 }
