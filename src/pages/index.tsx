e53d1a3993e1f8f7976b66c17e7d2931087c8658
CristianoV <cristianoviieira@gmail.com>
2023-02-09 19:05:02 -0300

    feat: implementa novo layout da pagina principal

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,145 +1,119 @@
 /* eslint-disable @next/next/no-img-element */
 import Head from 'next/head';
 import styles from '../styles/home.module.scss';
+import Logo from '../../public/logoName.svg';
+import Cristiano from '../../public/cristianoPicture.svg';
 import Image from 'next/image';
 
 import { GetStaticProps } from 'next';
 import { createClient } from '../services/prismic';
 import { RichText } from 'prismic-dom';
+import { BsGithub, BsLinkedin } from 'react-icons/bs';
 import Link from 'next/link';
+import { FaFileDownload } from 'react-icons/fa';
 
-type Content = {
-  title: string;
-  titleContent: string;
-  linkAction: string;
-  image1: string;
+// type Content = {
+//   title: string;
+//   titleContent: string;
+//   linkAction: string;
+//   image1: string;
 
-  title2: string;
-  titleContent2: string;
-  linkAction2: string;
-  image2: string;
+//   title2: string;
+//   titleContent2: string;
+//   linkAction2: string;
+//   image2: string;
 
-  title3: string;
-  titleContent3: string;
-  linkAction3: string;
-  image3: string;
-};
+//   title3: string;
+//   titleContent3: string;
+//   linkAction3: string;
+//   image3: string;
+// };
 
-interface ContentProps {
-  content: Content;
-}
+// interface ContentProps {
+//   content: Content;
+// }
 
-export default function Home({ content }: ContentProps) {
+export default function Home() {
   return (
     <>
       <Head>
         <title>Portifolio CristianoV</title>
       </Head>
       <main className={styles.container}>
-        {/* <div className={styles.containerHeader}>
-          <section className={styles.ctaText}>
-            <h1>{content.title}</h1>
-            <span>{content.titleContent}</span>
-            <a href={content.linkAction}>
-              <button>COMEÇAR AGORA!</button>
+        <div>
+          <Image src={Logo} alt='Logo' />
+          <h1>DESENVOLVEDOR FULL STACK</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur. Venenatis urna quis morbi
+            viverra.
+          </p>
+          <nav>
+            <Link href='/contato'>ME CONTRATE</Link>
+            <Link href='/contato'>PUBLICAÇÕES</Link>
+          </nav>
+          <div>
+            <Link href='https://github.com/CristianoV'>
+              <BsGithub size={32} color='#3EFA52' />
+              GitHub
+            </Link>
+            <Link href='https://www.linkedin.com/in/cristianov/'>
+              <BsLinkedin size={32} color='#3EFA52' />
+              LinkedIn
+            </Link>
+            <a href='/files/CristianoVieira.pdf' download>
+              <FaFileDownload size={26} color='#3EFA52' />
+              Download Curriculo
             </a>
-          </section>
-
-          <img src={content.image1} alt='Conteúdos Sujeito Programador' />
-        </div>
-
-        <hr className={styles.divisor} /> */}
-
-        <div className={styles.sectionContent}>
-          <section>
-            <h2>{content.title}</h2>
-            <span>{content.titleContent}</span>
-          </section>
-
-          <img src={content.image1} alt={content.title} />
-        </div>
-
-        <hr className={styles.divisor} />
-
-        <div className={styles.sectionContent}>
-          <img src={content.image2} alt={content.title2} />
-          <section>
-            <h2>{content.title2}</h2>
-            <span>{content.titleContent2}</span>
-          </section>
-
-        </div>
-
-        <hr className={styles.divisor} />
-
-        <div className={styles.sectionContent}>
-          <section>
-            <h2>{content.title3}</h2>
-            <span>{content.titleContent3}</span>
-          </section>
-
-          <img src={content.image3} alt={content.title3} />
-        </div>
-        
-
-        <div className={styles.nextLevelContent}>
-          {/* <Image quality={100} src={techsImage} alt="Tecnologias" /> */}
-          <h2>
-            Para mais informações
-          </h2>
-          <span>Você pode entrar em contato clicando nesse botão e enviando o seu email</span>
-          <Link href='/contato'>
-            <button>Mande seu Email</button>
-          </Link>
+          </div>
         </div>
       </main>
     </>
   );
 }
 
-export const getStaticProps: GetStaticProps = async ({ previewData }) => {
-  const client = createClient({ previewData });
+// export const getStaticProps: GetStaticProps = async ({ previewData }) => {
+//   const client = createClient({ previewData });
 
-  const page = await client.getSingle('home');
+//   const page = await client.getSingle('home');
 
-  const {
-    title1,
-    sub_title1,
-    link_action1,
-    image1,
+//   const {
+//     title1,
+//     sub_title1,
+//     link_action1,
+//     image1,
 
-    title2,
-    sub_title2,
-    link_action2,
-    image2,
+//     title2,
+//     sub_title2,
+//     link_action2,
+//     image2,
 
-    title3,
-    sub_title3,
-    link_action3,
-    image3,
-  } = page.data;
+//     title3,
+//     sub_title3,
+//     link_action3,
+//     image3,
+//   } = page.data;
 
-  const content = {
-    title: RichText.asText(title1),
-    titleContent: RichText.asText(sub_title1),
-    linkAction: link_action1.url,
-    image1: image1.url,
+//   const content = {
+//     title: RichText.asText(title1),
+//     titleContent: RichText.asText(sub_title1),
+//     linkAction: link_action1.url,
+//     image1: image1.url,
 
-    title2: RichText.asText(title2),
-    titleContent2: RichText.asText(sub_title2),
-    linkAction2: link_action2.url,
-    image2: image2.url,
+//     title2: RichText.asText(title2),
+//     titleContent2: RichText.asText(sub_title2),
+//     linkAction2: link_action2.url,
+//     image2: image2.url,
 
-    title3: RichText.asText(title3),
-    titleContent3: RichText.asText(sub_title3),
-    linkAction3: link_action3.url,
-    image3: image3.url,
-  };
+//     title3: RichText.asText(title3),
+//     titleContent3: RichText.asText(sub_title3),
+//     linkAction3: link_action3.url,
+//     image3: image3.url,
+//   };
 
-  return {
-    props: {
-      content,
-    },
-    revalidate: 60 * 60 * 12, // 12 hours
-  };
-};
+//   return {
+//     props: {
+//       content,
+//     },
+//     revalidate: 60 * 60 * 12, // 12 hours
+//   };
+// };
