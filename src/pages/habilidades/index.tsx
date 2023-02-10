6d0d62ae1349dd19ed6884e1aca82add985adcaf
CristianoV <cristianoviieira@gmail.com>
2023-02-09 22:20:30 -0300

    feat: adiciona novas rotas de paginas

---
+++ src/pages/habilidades/index.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.scss';
+
+export default function habilidades() {
+  return (
+    <div className={styles.container}>
+      <h1>habilidades</h1>
+    </div>
+  )
+}
---
+++ src/pages/publicacoes/index.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.scss';
+
+export default function Publicacoes() {
+  return (
+    <div className={styles.container}>
+      <h1>Publicações</h1>
+    </div>
+  )
+}
