fe00917ac673353e6d40729c9c2de1493186a04d
CristianoV <cristianoviieira@gmail.com>
2023-02-15 12:53:29 -0300

    feat: remove linhas comentadas

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,12 @@
-/* eslint-disable @next/next/no-img-element */
 import Head from 'next/head';
 import styles from '../styles/home.module.scss';
 import Logo from '../../public/logoName.svg';
 import Cristiano from '../../public/cristianoPicture.svg';
 import Image from 'next/image';
-
-import { GetStaticProps } from 'next';
-import { createClient } from '../services/prismic';
-import { RichText } from 'prismic-dom';
 import { BsGithub, BsLinkedin } from 'react-icons/bs';
 import Link from 'next/link';
 import { FiDownload } from 'react-icons/fi';
-
-// type Content = {
-//   title: string;
-//   titleContent: string;
-//   linkAction: string;
-//   image1: string;
-
-//   title2: string;
-//   titleContent2: string;
-//   linkAction2: string;
-//   image2: string;
-
-//   title3: string;
-//   titleContent3: string;
-//   linkAction3: string;
-//   image3: string;
-// };
-
-// interface ContentProps {
-//   content: Content;
-// }
+import { GetStaticProps } from 'next';
 
 export default function Home() {
   return (
@@ -41,12 +16,7 @@ export default function Home() {
       </Head>
       <main className={styles.container}>
         <div>
-          <Image
-            src={Logo}
-            alt='Logo'
-            placeholder='blur'
-            blurDataURL='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mN0vQgAAWEBGHsgcxcAAAAASUVORK5CYII='
-          />
+          <Image src={Logo} alt='Logo' />
           <h1>DESENVOLVEDOR FULL STACK</h1>
           <p>
             Iniciando minha carreira como Desenvolvedor, busco desafios que irão
@@ -69,7 +39,7 @@ export default function Home() {
               LinkedIn
             </Link>
             <a href='/files/CristianoVieira.pdf' download>
-              <FiDownload size={21} />
+              <FiDownload size={32} />
               Baixe meu currículo
             </a>
           </div>
@@ -80,49 +50,9 @@ export default function Home() {
   );
 }
 
-// export const getStaticProps: GetStaticProps = async ({ previewData }) => {
-//   const client = createClient({ previewData });
-
-//   const page = await client.getSingle('home');
-
-//   const {
-//     title1,
-//     sub_title1,
-//     link_action1,
-//     image1,
-
-//     title2,
-//     sub_title2,
-//     link_action2,
-//     image2,
-
-//     title3,
-//     sub_title3,
-//     link_action3,
-//     image3,
-//   } = page.data;
-
-//   const content = {
-//     title: RichText.asText(title1),
-//     titleContent: RichText.asText(sub_title1),
-//     linkAction: link_action1.url,
-//     image1: image1.url,
-
-//     title2: RichText.asText(title2),
-//     titleContent2: RichText.asText(sub_title2),
-//     linkAction2: link_action2.url,
-//     image2: image2.url,
-
-//     title3: RichText.asText(title3),
-//     titleContent3: RichText.asText(sub_title3),
-//     linkAction3: link_action3.url,
-//     image3: image3.url,
-//   };
-
-//   return {
-//     props: {
-//       content,
-//     },
-//     revalidate: 60 * 60 * 12, // 12 hours
-//   };
-// };
+export const getStaticProps: GetStaticProps = async ({ previewData }) => {
+  return {
+    props: {},
+    revalidate: 60 * 60 * 12, // 12 hours
+  };
+};
