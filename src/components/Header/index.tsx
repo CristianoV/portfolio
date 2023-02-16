a33e8eb695e877282065a7ced6e1843301a05433
CristianoV <cristianoviieira@gmail.com>
2023-02-15 23:38:10 -0300

    feat: adiciona conteiner

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,19 +7,21 @@ import Image from 'next/image';
 
 export function Header() {
   return (
-    <header className={styles.container}>
-      <Link href='/'>
-        <Image src={Logo} alt='Logo' />
-      </Link>
-
-      <nav>
-        <ActiveLink href='/' texto='Início' />
-        <ActiveLink href='/publicacoes' texto='Publicações' />
-        <ActiveLink href='/sobre' texto='Sobre' />
-        <Link className={styles.readyButton} type='button' href='/contato'>
-          <p>ME CONTRATE</p>
+    <div className={styles.containerHeader}>
+      <header className={styles.container}>
+        <Link href='/'>
+          <Image src={Logo} alt='Logo' />
         </Link>
-      </nav>
-    </header>
+
+        <nav>
+          <ActiveLink href='/' texto='Início' />
+          <ActiveLink href='/publicacoes' texto='Publicações' />
+          <ActiveLink href='/sobre' texto='Sobre' />
+          <Link className={styles.readyButton} type='button' href='/contato'>
+            <p>ME CONTRATE</p>
+          </Link>
+        </nav>
+      </header>
+    </div>
   );
 }
