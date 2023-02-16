b852bee26514ac971cfc44a801d244c44e91489b
CristianoV <cristianoviieira@gmail.com>
2023-02-16 19:01:23 -0300

    feat: adiciona componentes na page de sobre

--- src/pages/sobre/index.tsx
+++ src/pages/sobre/index.tsx
@@ -1,9 +1,12 @@
 import styles from './styles.module.scss';
+import SobrePage from '../../components/SobrePage';
+import SkillsPage from '../../components/SkillsPage';
 
 export default function sobre() {
   return (
     <div className={styles.container}>
-      <h1>sobre</h1>
+      <SobrePage />
+      <SkillsPage />
     </div>
   )
 }
