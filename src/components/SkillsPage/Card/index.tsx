ced95233cabc79b2f72d3f99aaac5b3351bd0214
CristianoV <cristianoviieira@gmail.com>
2023-02-16 12:56:37 -0300

    feat: implementa sistema de nivel e imagens no component

--- src/components/SkillsPage/Card/index.tsx
+++ src/components/SkillsPage/Card/index.tsx
@@ -1,9 +1,31 @@
 import styles from './styles.module.scss';
+import Image, { StaticImageData } from 'next/image';
 
-export default function Card() {
+type DivStyle = {
+  [key: number]: string;
+};
+
+export default function Card({
+  level,
+  img,
+}: {
+  level: number;
+  img: StaticImageData;
+}) {
+  const divStyle: DivStyle = {
+    6: styles.level6,
+    7: styles.level7,
+    8: styles.level8,
+    9: styles.level9,
+  };
+
   return (
     <div className={styles.container}>
-      
+      <Image src={img} alt='NodeJS' />
+      <div className={divStyle[level]}>
+        <div></div>
+      </div>
+      <h1>NodeJS</h1>
     </div>
   );
 }
