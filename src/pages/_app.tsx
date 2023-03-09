476af8bdee4054768883e6508566e20d744f383d
CristianoV <cristianoviieira@gmail.com>
2023-03-09 14:10:37 -0300

    feat: adiciona footer a todas as paginas

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
-import type { AppProps } from 'next/app'
-import '../styles/globals.scss'
-import { Header } from '../components/Header'
+import type { AppProps } from 'next/app';
+import '../styles/globals.scss';
+import { Header } from '../components/Header';
+import Footer from '../components/Footer';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Header />
       <Component {...pageProps} />
+      <Footer />
     </>
-  )
+  );
 }
