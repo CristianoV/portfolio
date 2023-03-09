e2246fbbce2b005f25db2fd5c36ef92503bebfde
CristianoV <cristianoviieira@gmail.com>
2023-03-09 20:21:17 -0300

    feat: implementa feach pelo lado do servidor

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,30 @@ import SobrePage from '../components/SobrePage';
 import SkillsPage from '../components/SkillsPage';
 import { GetStaticProps } from 'next';
 import Contact from '../components/Contact';
+import Projects from '../components/Projects';
+import { createClient } from '../services/prismic';
+import { RichText } from 'prismic-dom';
+import * as prismic from '@prismicio/client';
 
-export default function Home() {
+type Post = {
+  slug: string;
+  title: string;
+  image: string;
+  description: string;
+  updatedAt: string;
+};
+
+interface PostsProps {
+  posts: Post[];
+  page: string;
+  totalPage: string;
+}
+
+export default function Home({
+  posts: postsBlog,
+  page,
+  totalPage,
+}: PostsProps) {
   return (
     <>
       <Head>
@@ -22,6 +44,7 @@ export default function Home() {
         <SkillsPage />
       </div>
       <div className={styles.background}>
+        <Projects posts={postsBlog} />
         <Contact />
       </div>
     </>
@@ -29,8 +52,43 @@ export default function Home() {
 }
 
 export const getStaticProps: GetStaticProps = async ({ previewData }) => {
+  const client = createClient({ previewData });
+
+  const response = await client.query(
+    prismic.predicate.at('document.type', 'post'),
+    {
+      pageSize: 6,
+      fetch: ['post.title', 'post.description', 'post.image'],
+      orderings: ['my.post.date desc'],
+    }
+  );
+
+  const posts = response.results.map((post) => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      description:
+        post.data.description.find(
+          (content: { type: string }) => content.type === 'paragraph'
+        )?.text ?? '',
+      image: post.data.image.url,
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        'pt-BR',
+        {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric',
+        }
+      ),
+    };
+  });
+
   return {
-    props: {},
+    props: {
+      posts,
+      page: response.page,
+      totalPage: response.total_pages,
+    },
     revalidate: 60 * 60 * 12, // 12 hours
   };
 };
