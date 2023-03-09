2286e2fee00978ecc127f514f08167c8c51d9ec8
CristianoV <cristianoviieira@gmail.com>
2023-03-09 20:19:27 -0300

    style: adiciona style para animação cordenada

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,19 +6,28 @@ import Logo from '../../../public/logoSite.svg';
 import Link from 'next/link';
 import Image from 'next/image';
 
+interface CustomStyle extends React.CSSProperties {
+  '--i': number;
+}
+
 export function Header() {
   return (
-    <div className={styles.containerHeader} id="header">
+    <div className={styles.containerHeader} id='header'>
       <header className={styles.container}>
         <Link href='/'>
           <Image src={Logo} alt='Logo' />
         </Link>
 
         <nav>
-          <ActiveLink href='/' texto='Início' />
-          <ActiveLink href='/publicacoes' texto='Publicações' />
-          <ActiveLink href='/#sobre' texto='Sobre' />
-          <a className={styles.readyButton} type='button' href='/#Contact'>
+          <ActiveLink href='/' texto='Início' number={1} />
+          <ActiveLink href='/publicacoes' texto='Publicações' number={2} />
+          <ActiveLink href='/#sobre' texto='Sobre' number={3} />
+          <a
+            className={styles.readyButton}
+            type='button'
+            href='/#Contact'
+            style={{ '--i': 4 } as CustomStyle}
+          >
             <p>ME CONTRATE</p>
           </a>
         </nav>
