6ad34adbd47a44f53f9671a65ce797fb26890b98
CristianoV <cristianoviieira@gmail.com>
2023-03-12 02:57:11 -0300

    feat: adiciona link de redirecionamento para linkedin de desenvolvedores

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,13 +3,24 @@ import styles from './styles.module.scss';
 import Logo from '../../../public/footerLogo.svg';
 import up from '../../../public/up.svg';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Footer() {
   return (
     <footer className={styles.containerFooter}>
       <div className={styles.footer}>
         <Image src={Logo} alt='Logo' />
-        <div>Layout by: Alisson Birck</div>
+        <div>
+          <Link
+            href='https://www.linkedin.com/in/alissonbirck/'
+            target='_blank'
+          >
+            Layout by: Alisson Birck
+          </Link>
+          <Link href='https://www.linkedin.com/in/cristianov/' target='_blank'>
+            Developed by: Cristiano Vieira
+          </Link>
+        </div>
         <a href='/#header'>
           <Image src={up} alt='Logo' />
         </a>
