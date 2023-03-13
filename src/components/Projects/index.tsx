2bcf349a52abff7babb89db7c1b90644aabf94f7
CristianoV <cristianoviieira@gmail.com>
2023-03-13 13:52:58 -0300

    feat: adiciona função que divide os projetos

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -4,6 +4,7 @@ import Card from '../Card';
 type Post = {
   slug: string;
   title: string;
+  subTitle: string;
   image: string;
   description: string;
   updatedAt: string;
@@ -14,21 +15,38 @@ interface PostsProps {
 }
 
 export default function Projects({ posts }: PostsProps) {
+  const postsChunks: Post[][] = posts.reduce(
+    (resultArray: Post[][], item: Post, index: number) => {
+      const chunkIndex = Math.floor(index / (posts.length / 2));
+
+      if (!resultArray[chunkIndex]) {
+        resultArray[chunkIndex] = [];
+      }
+
+      resultArray[chunkIndex].push(item);
+
+      return resultArray;
+    },
+    []
+  );
+
   return (
     <div className={styles.container}>
       <section>
         <h1>Projetos</h1>
-        <div>
-          {posts.map((post) => (
-            <Card
-              key={post.slug}
-              title={post.title}
-              image={post.image}
-              description={post.description}
-              slug={post.slug}
-            />
-          ))}
-        </div>
+        {postsChunks.map((chunk: Post[], index: number) => (
+          <div key={index} className={styles.cardContainer}>
+            {chunk.map((post: Post) => (
+              <Card
+                key={post.slug}
+                title={post.title}
+                image={post.image}
+                description={post.subTitle}
+                slug={post.slug}
+              />
+            ))}
+          </div>
+        ))}
       </section>
     </div>
   );
